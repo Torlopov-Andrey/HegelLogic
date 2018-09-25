fed359273b342db13ae08e25274fee01d8ed4b1c
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-09-26 00:26:56 +0300

    BuingNothing

---
+++ BuingNothing.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+print("W.F.Gegel - Logic.")
+
+class Being {
+	var thereIs: Bool = true
+	var parents: [Being] = []
+	
+	init(parents: [Being] = []) {
+		self.parents = parents
+		self.parents.append(self)
+	}
+	
+	func transition() -> Being {
+		return Nothing(parents: parents)
+	}
+	
+	func think() -> Being {
+		return transition()
+	}
+	
+	func description() -> String {
+		return "Buing with parents [\(self.parents.count)]"
+	}
+}
+
+class Nothing: Being {
+	
+	override func transition() -> Being {
+		return Being(parents: parents)
+	}
+	
+	override func description() -> String {
+		return "Nothing with parents [\(self.parents.count)]"
+	}
+}
+
+let buing = Being()
+let nothing = buing.think().think().think().think()
