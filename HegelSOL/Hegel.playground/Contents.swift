3d5139da65f377367a4290cb9159e526628054ef
Torlopov Andrey <Torlopov.andrey@gmail.com>
2018-09-26 09:19:21 +0300

    update

--- HegelSOL/Hegel.playground/Contents.swift
+++ HegelSOL/Hegel.playground/Contents.swift
@@ -6,34 +6,65 @@ class Being {
 	var thereIs: Bool = true
 	var parents: [Being] = []
 	
-	init(parents: [Being] = []) {
+	private var customDescription: String?
+	
+	init(parents: [Being] = [], customDescription: String? = nil) {
 		self.parents = parents
 		self.parents.append(self)
 	}
 	
 	func transition() -> Being {
-		return Nothing(parents: parents)
+		//FIXME: точное сравнение типов
+		let condition = parents.filter { $0 is Being }.count > 0 && parents.filter { $0 is Nothing }.count > 0
+		return condition ? Becoming(parents: parents) : Nothing(parents: parents)
 	}
 	
 	func think() -> Being {
 		return transition()
 	}
 	
+	func negation() -> Being {
+		return transition()
+	}
+	
 	func description() -> String {
-		return "Buing with parents [\(self.parents.count)]"
+		return customDescription == nil ? "Buing with parents [\(self.parents.count)]" : customDescription!
 	}
 }
 
 class Nothing: Being {
-	
+
 	override func transition() -> Being {
-		return Being(parents: parents)
+		let condition = parents.filter { $0 is Being }.count > 0 && parents.filter { $0 is Nothing }.count > 0
+		return condition ? Becoming(parents: parents) : Being(parents: parents)
 	}
 	
 	override func description() -> String {
-		return "Nothing with parents [\(self.parents.count)]"
+		return customDescription == nil ? "Nothing with parents [\(self.parents.count)]" : customDescription!
+	}
+}
+
+class Becoming: Being {
+	
+	func momentOfOccurrence() -> Nothing {
+		return Nothing(parents: parents, customDescription: "Moment of occurrence with parents: [\(self.parents.count)]")
+	}
+	
+	func momentOfArrival() -> Being {
+		return Being(parents: parents, customDescription: "Moment of arrival with parents: [\(self.parents.count)]")
+	}
+		
+	override func description() -> String {
+		return "Becoming with parents [\(self.parents.count)]"
 	}
 }
 
 let buing = Being()
 let nothing = buing.think().think().think().think()
+
+print(buing.description())
+print(nothing.description())
+
+if let nothing = nothing as? Becoming {
+	nothing.momentOfArrival()
+}
